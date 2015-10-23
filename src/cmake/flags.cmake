e633fee1f5cd90c89be100fbeee3fea829bdd248
Pavel Avdeyev <avdeevp@gmail.com>
2015-10-23 15:21:52 -0400

    Recover tree cmake done

--- src/cmake/flags.cmake
+++ src/cmake/flags.cmake
@@ -1,3 +1,21 @@
+# -*- cmake -*-
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.7")
+        message(SEND_ERROR "GCC version must be at least 4.7!")
+        return()
+    endif()
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    message(SEND_ERROR "Support for Clang will be in next versions!")
+    return()
+    #if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS "3.2")
+        #message(SEND_ERROR "Clang version must be at least 3.2!")
+        #return()
+    #endif()
+else()
+    message(SEND_ERROR "You are using an unsupported compiler! Compilation has only been tested with Clang and GCC.")
+    return()
+endif()
 
 
 # So we can check build type throughout the script

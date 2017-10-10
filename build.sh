56a758f19da94f23321d8ab4f953619ddf766067
ANekhai <30994106+ANekhai@users.noreply.github.com>
2017-10-10 14:13:04 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -6,18 +6,25 @@ HOME_DIR=$PWD
 ## MAIN FUNCTION ############
 #############################
 
+if [ "$#" -ne 1 ]; then
+	echo "./build.sh {option}
+Where option is a build type for MGRA.
+Pick one: None, Debug, Release, RelWithAsserts, RelWithDebInfo
+	"
+	exit
+fi
+BUILD_TYPE=$1
+
 # Make Build Directory
+rm -rf build
 mkdir build
 BUILD_DIR=${HOME_DIR}/build
 
 # Copy CMakeLists.txt and build MGRA
 cp ${HOME_DIR}/CMakeLists.txt ${BUILD_DIR}
 cd ${BUILD_DIR}
-cmake -DCMAKE_BUILD_TYPE=Debug ${HOME_DIR}
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${HOME_DIR}
 make
-
-# Run tests
-# ./MGRA_test
 
 #############################
 ## END ######################

43a30ccd7576d3ac86718473641926abb34119c9
ANekhai <30994106+ANekhai@users.noreply.github.com>
2017-10-10 13:49:30 -0400

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HOME_DIR=$PWD
+
+#############################
+## MAIN FUNCTION ############
+#############################
+
+# Make Build Directory
+mkdir build
+BUILD_DIR=${HOME_DIR}/build
+
+# Copy CMakeLists.txt and build MGRA
+cp ${HOME_DIR}/CMakeLists.txt ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake -DCMAKE_BUILD_TYPE=Debug ${HOME_DIR}
+make
+
+# Run tests
+./MGRA_test
+
+#############################
+## END ######################
+#############################
+
